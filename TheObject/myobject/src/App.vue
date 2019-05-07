7bea81c15ccaa8935088c243c45d747a217fb8f2
DESKTOP-5TBM399\lx <13343647507@163.com>
2019-05-07 16:18:30 +0800

    更改了基础的 font-size

--- TheObject/myobject/src/App.vue
+++ TheObject/myobject/src/App.vue
@@ -10,13 +10,14 @@
 import '../build/resize.js' // 引入 resize.js 文件
 
 import 'jquery/dist/jquery.min'
+import Header from "./components/Header";
 import Footer from "./components/Footer";
  // 引入 bootstrap.min css/js 文件
 // import 'node_modules/bootstrap/dist/js/bootstrap.min.js'
 
 export default {
   name: 'App',
-  components: {Footer}
+  components: {Header,Footer}
 }
 </script>
 
@@ -28,6 +29,7 @@ export default {
   html,body{
     width: 100%;
     height: 100%;
+    font-size: 14px;
   }
 
   #app{
--- TheObject/myobject/src/components/Header.vue
+++ TheObject/myobject/src/components/Header.vue
@@ -11,5 +11,13 @@ export default {
 </script>
 
 <style scoped>
-    
+    .header{
+        width: 100%;
+        height: 10%;
+        background-color: rgb(49, 144, 232);
+        position: fixed;
+        left: 0;
+        top: 0;
+        right: 0;
+    }
 </style>
