82921f1013dcc20867dd19b7d1063ae0314cd313
541229920 <49836889+541229920@users.noreply.github.com>
2019-05-07 19:10:23 +0800

    no message

--- TheObject/myobject/src/components/Sou.vue
+++ TheObject/myobject/src/components/Sou.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="Sou">Sou</div>
+  <div class="Sou">Sou</div>
 </template>
 
 <script>
-    export default {
-        name: "Sou"
-    }
+  export default {
+    name: "Sou",
+    methods: {}
+  }
 </script>
 
 <style scoped>
-.Sou{
-  width: 100%;
-  height:100%;
-  background: rgb(245,245,245);
-  margin-top: 1.9rem;
-}
+  .Sou {
+    width: 100%;
+    height: 100%;
+    background: rgb(245, 245, 245);
+    margin-top: 1.9rem;
+  }
 </style>
