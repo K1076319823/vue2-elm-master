4840ec2567cfa45e16a3dbb880f2a732a6991187
DESKTOP-5TBM399\lx <13343647507@163.com>
2019-05-07 13:34:40 +0800

    promise 以及 vuex 下载

--- TheObject/myobject/src/main.js
+++ TheObject/myobject/src/main.js
@@ -1,5 +1,7 @@
 
 import Vue from 'vue'
+import 'es6-promise/auto' 
+import Vuex from 'vuex' // 引入 vuex 以及其依赖 的 promise模块
 import App from './App'
 import router from './router'
 import $ from 'jquery'
