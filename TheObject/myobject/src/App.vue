9fa696aba7fdfcabea0a02a6b10ee08e3e4e0e1f
DESKTOP-5TBM399\lx <18238607162zh@163.com>
2019-05-07 11:52:18 +0800

    app.vue

--- TheObject/myobject/src/App.vue
+++ TheObject/myobject/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-
+    <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
-
 <script>
+
 export default {
   name: 'App'
 }
