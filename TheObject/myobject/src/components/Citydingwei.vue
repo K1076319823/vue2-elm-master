e28285117e2864f3742413761f50652721e3a5cd
DESKTOP-5TBM399\lx <18238607162zh@163.com>
2019-05-08 22:04:08 +0800

    dingwei

--- TheObject/myobject/src/components/Citydingwei.vue
+++ TheObject/myobject/src/components/Citydingwei.vue
@@ -8,6 +8,17 @@
    </div>
     <div class="hod">
       <p>热门城市</p>
+      <div v-for="lt in Hotcity ">
+       <router-link  :to="{}"><div class="hname">{{lt.name}}</div></router-link>
+      </div>
+    </div>
+    <div v-for="(item ,i) in fc">
+      <div class="clearfix abc" >
+        <p>{{i}}</p>
+        <div class="allcity" v-for="addr in item">
+          {{addr.name}}
+        </div>
+      </div>
 
     </div>
   </div>
@@ -17,22 +28,45 @@
   import  Vue from 'vue';
     import Starttop from "./Starttop";
     export default {
-        name: "Citydingwei",
-      data(){
+      name: "Citydingwei",
+      data() {
         return {
           //所有商品的数组
           Hotcity: {},
-        }},
+          Grocity: {},
+          citykey: {},
+          newCity: {}
+        }
+      },
       components: {Starttop},
-      comments:{
+      comments: {
         Starttop
       },
-      computed: {},
-      created() {
-        Vue.axios.get('https://elm.cangdu.org/v1/cities?type=hot' ).then((response) => {
-          console.log(response.data);
-        })
-      }
+      computed:{
+        fc() {
+          this.citykey = Object.keys(this.Grocity);
+          this.citykey.sort();
+          console.log(this.citykey);
+            for (let i = 0; i < this.citykey.length;i++) {
+            const index = this.citykey[i];
+            this.newCity[index]=this.Grocity[index];
+          }
+            console.log(this.newCity);
+            return this.newCity;
+          }
+        },
+        created() {
+          Vue.axios.get('https://elm.cangdu.org/v1/cities?type=hot').then((response) => {
+            console.log(response.data);
+            this.Hotcity = response.data;
+          });
+          Vue.axios.get('https://elm.cangdu.org/v1/cities?type=group').then((response) => {
+            // console.log(response.data);
+            this.Grocity = response.data;
+          });
+
+        }
+
     }
 </script>
 
@@ -54,5 +88,52 @@
   }
   .hod{
     background-color: #fff;
+    margin-bottom:1rem;
+  }
+  .hod p{
+    margin-bottom:0;
+    line-height: 1.5rem;
+    border-top:1px #ccc solid;
+  }
+  .hname{
+    width:25%;
+    box-sizing: border-box;
+    float:left;
+    text-align:center;
+    background-color: #fff;
+    font-size:0.7rem;
+    color: rgb(49, 144, 232);
+    line-height: 1.5rem;
+    border:1px #ccc solid;
+  }
+  .allcity{
+    width:25%;
+    box-sizing: border-box;
+    float:left;
+    text-align:center;
+
+    font-size:0.5rem;
+    color: #ccc;
+    line-height: 1.5rem;
+    border:1px #ccc solid;
+    text-align:center;
+    height:1.5rem;
+    /*text-overflow: ellipsis;*/
+    overflow: hidden;
+
+
+  }
+  .abc{
+    margin:1rem 0;
+    background-color: #fff;
+    border:1px red solid;
+  }
+  .abc p{
+    background-color: red;
+    margin-bottom:0;
+    margin-top:1rem;
+  }
+  .allcity{
+    border:1px #ccc solid;
   }
 </style>
