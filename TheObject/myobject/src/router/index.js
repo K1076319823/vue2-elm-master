f3c12bef81734efe41d9246f0ff2d4e12e61ea2c
DESKTOP-5TBM399\lx <18238607162zh@163.com>
2019-05-13 11:33:32 +0800

    新路由

--- TheObject/myobject/src/router/index.js
+++ TheObject/myobject/src/router/index.js
@@ -14,14 +14,10 @@ import Footer from '../components/Footer';
 import FserviceC from '../components/FserviceC';
 import Login from '../components/Login';
 import Reset from '../components/Login_reset';
-import Classify from '../components/Classify'; 
-// 下面这两个是 全局动画组件和 账户信息 组件.
-// import Bounce from '../components/Bounce';
-import Account from '../components/Account';
-// 账户信息组件 ----> 用户名 ----> 修改用户名
-import ChangeUser from '../components/ChangeUser'
-// 编辑地址
-import Addressee from '../components/Addressee'
+import Classify from '../components/Classify';
+import CitySou from '../components/CitySou';
+import Balance from "../components/Balance";
+import Baspask from "../components/Baspask";
 
 export default new Router({
   routes: [
@@ -31,16 +27,16 @@ export default new Router({
     { path: '/Mai', component: Mai },
     { path: '/Sou', component: Sou },
     { path: '/Ding', component: Ding },
-    { path: '/My', component: My },
+    { path: '/My', component: My},
+    {path:'/Balance',component:Balance},
+    {path:'/Baspask',component:Baspask},
     { path: '/Citydingwei', component: Citydingwei },
     { path: '/FserviceC', component: FserviceC },
     { path: '/Login',component: Login },
     { path: '/Reset', component: Reset },
     { path: '/Classify', component: Classify },
-    // { path: '/Bounce', component: Bounce },
-    { path: '/Account',component: Account },
-    { path: '/ChangeUser', component: ChangeUser },
-    { path: '/Addressee',component: Addressee }
+    {path:'/CitySou',component:CitySou},
+
   ]
 })
 
